ff236fc1fcaad0f5ac93fbaa4ef7cc3609c61c79
alicembera <mberaalice7@gmail.com>
2024-07-18 21:44:24 +0200

    updating docker-compose file and config

--- src/api/config.py
+++ src/api/config.py
@@ -4,11 +4,12 @@ ONE_DAY_SEC = 24*60*60
 
 ONE_WEEK_SEC = ONE_DAY_SEC*7
 
-XGBOOST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/xgboost.joblib"
+XGBOOST_URL = "https://raw.githubusercontent.com/alicembera/Building-Machine-Learning-API-s-With-FastAPI/main/dev/models/xgboost.joblib"
+ 
 
-RANDOM_FOREST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/random_forest.joblib"
+RANDOM_FOREST_URL = "https://raw.githubusercontent.com/alicembera/Building-Machine-Learning-API-s-With-FastAPI/main/dev/models/random_forest.joblib"
 
-ENCODER_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/encoder.joblib"
+ENCODER_URL = "https://raw.githubusercontent.com/alicembera/Building-Machine-Learning-API-s-With-FastAPI/main/dev/models/encoder.joblib"
 
 BASE_DIR = './'  # Where Unicorn server runs from
 
